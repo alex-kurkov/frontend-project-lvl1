358a5f8d59e612b325f21414c907efa7c8de4dfc
Alex Kurkov <alexkourkov@yandex.ru>
2020-09-05 15:35:36 +0500

    change brain-games.js file

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import { getUserName, greetUser } from '../src/cli.js';
+import { greetAndGetUserName } from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-const userName = getUserName();
-greetUser(userName);
+const user = greetAndGetUserName();

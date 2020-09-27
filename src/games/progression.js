cf3b3621a1c31bb41f5b4615118f182f8f1705bd
alex-kurkov <alexkourkov@yandex.ru>
2020-09-27 12:48:32 +0500

    refactor progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,23 +1,23 @@
 import startGame from '../index.js';
 import getRandomIntFromRange from '../utils/getRandomIntFromRange.js';
 
-const makeProgression = (nStart, nStep, nLength) => {
-  const arr = [nStart];
-  for (let i = 1; i < nLength; i += 1) {
-    arr.push(nStart + (nStep * i));
+const makeProgression = (firstElem, step, length) => {
+  const arr = [firstElem];
+  for (let i = 1; i < length; i += 1) {
+    arr.push(firstElem + (step * i));
   }
   return arr;
 };
-const progressionLength = 10;
+const PROGRESSION_LENGTH = 10;
 
 export default () => {
   startGame('What number is missing in the progression?', () => {
     const progression = makeProgression(
       getRandomIntFromRange(0, 20),
       getRandomIntFromRange(2, 9),
-      progressionLength,
+      PROGRESSION_LENGTH,
     );
-    const idxHidden = getRandomIntFromRange(0, progressionLength - 1);
+    const idxHidden = getRandomIntFromRange(0, PROGRESSION_LENGTH - 1);
     const correctAnswer = String(progression.splice(idxHidden, 1, '..'));
     return { correctAnswer, question: progression.join(' ') };
   });

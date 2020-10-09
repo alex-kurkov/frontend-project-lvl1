770bd43bcfec2900e09d0f0827bb88af6a53912c
alex-kurkov <alexkourkov@yandex.ru>
2020-10-09 12:46:14 +0500

    fix progression.js naming

--- src/games/progression.js
+++ src/games/progression.js
@@ -2,18 +2,16 @@ import playGame from '../index.js';
 import getRandomIntFromRange from '../utils/getRandomIntFromRange.js';
 
 const taskDescription = 'What number is missing in the progression?';
-const makeProgression = (firstElement, step, length) => {
+const makeProgression = (first, step, length) => {
   const arr = [];
   for (let i = 0; i < length; i += 1) {
-    arr.push(firstElement + (step * i));
+    arr.push(first + (step * i));
   }
   return arr;
 };
 const progressionLength = 10;
 
 const generateRoundData = () => {
-  // перенес на этот уровень, чтобы генерировались разные прогрессии для 3
-  // последовательных вопросов
   const first = getRandomIntFromRange(0, 10);
   const step = getRandomIntFromRange(2, 9);
   const progression = makeProgression(
@@ -21,8 +19,8 @@ const generateRoundData = () => {
     step,
     progressionLength,
   );
-  const idxHidden = getRandomIntFromRange(0, progressionLength - 1);
-  const correctAnswer = String(progression.splice(idxHidden, 1, '..'));
+  const hiddenIndex = getRandomIntFromRange(0, progressionLength - 1);
+  const correctAnswer = String(progression.splice(hiddenIndex, 1, '..'));
   const question = progression.join(' ');
   return { correctAnswer, question };
 };
